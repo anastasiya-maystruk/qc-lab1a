edcd43bac99547aa48fb4d3327d106f8b6ed538c
Anastasiya Maystruk <nastyma@bigmir.net>
2014-12-24 11:00:42 +0200

    add checkstyle

--- src/test/java/com/home/SquareMatrixSorterTest.java
+++ src/test/java/com/home/SquareMatrixSorterTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class SquareMatrixSorterJUnitTests {
+public class SquareMatrixSorterTest {
 
     @Test(expected = Exception.class)
     public void testMainForForNull() throws Exception {
